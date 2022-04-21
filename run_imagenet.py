bfaf740f93547213f3b8a362a379ceb12228769c
nik-dim <dimitriadisnikolaos0@gmail.com>
2022-04-21 13:26:02 +0200

    minor changes

--- run_imagenet.py
+++ run_imagenet.py
@@ -1,35 +1,41 @@
-import os 
+import os
 import subprocess
-import time 
+import time
 
-from src.utils import date_second, date_millisecond, wait_for_proc_limit, wait_all_finish
+from src.utils import (
+    date_second,
+    date_millisecond,
+    wait_for_proc_limit,
+    wait_all_finish,
+)
 
 
 MAX_NO_PROC = 1
 
 SETTINGS = {
-    "UTIL_AWARE_SYSTOLIC": {"UTIL_COEF":1.0, "LAT_COEF":[1.0], "HW_TYPE":"systolic"},
+    "UTIL_AWARE_SYSTOLIC": {"UTIL_COEF": 1.0, "LAT_COEF": [1.0], "HW_TYPE": "systolic"},
 }
 
 STACKS = [
-    [1,0,1,0,1,0], 
+    [1, 0, 1, 0, 1, 0],
+    # [1, 0, 0, 1, 0, 0, 1, 0, 0]
 ]
 
-if __name__=="__main__":
+if __name__ == "__main__":
     exp_dir = "experiments/run-{}".format(date_second())
     os.system("mkdir -p {}".format(exp_dir))
 
     running_procs = []
     save_dirs = []
 
-    gpu_devices = [0,1,2,3]
+    gpu_devices = [0, 1, 2, 3]
 
     cnt = 0
     for baseline in SETTINGS:
         UTIL_COEF = SETTINGS[baseline]["UTIL_COEF"]
 
         HW_TYPE = SETTINGS[baseline]["HW_TYPE"]
-        
+
         LAT_COEF = 1.0
         for cs_stacks in STACKS:
             cs_stacks_str = " ".join([str(i) for i in cs_stacks])
@@ -38,8 +44,8 @@ if __name__=="__main__":
                 os.mkdir(SAVE_DIR)
 
                 cmd = f"python main.py \
-                    --train_batch_size 512 \
-                    --train_sgd_init_lr 0.02 \
+                    --train_batch_size 256 \
+                    --train_sgd_init_lr 0.06 \
                     --train_dataset imagenet\
                     --run_search 0 \
                     --run_train 1 \
@@ -47,7 +53,7 @@ if __name__=="__main__":
                     --ms_no_epoch {0} \
                     --cs_no_epoch {0} \
                     --ft_no_epoch {100} \
-                    --arch_dir experiments/run-2022_03_17-09_29_14/2022_03_17-09_29_14_991 \
+                    --arch_dir saved-models/stacks6 \
                     --util_coef {UTIL_COEF} \
                     --lat_coef {LAT_COEF} \
                     --gpu_device {gpu_devices[cnt%len(gpu_devices)]} \
@@ -61,7 +67,7 @@ if __name__=="__main__":
                 running_procs.append(p)
 
                 time.sleep(2)
-                
+
                 wait_for_proc_limit(running_procs, MAX_NO_PROC)
 
                 cnt += 1
